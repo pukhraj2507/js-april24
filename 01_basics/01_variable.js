ecfb89a72d7eb6f950b1a65e942d7bdbb22b6277
P_skunk <84781324+pukhraj2507@users.noreply.github.com>
2024-04-03 08:07:37 +0000

    variable and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "hithere@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 : not allowed
+
+accountEmail = "hmmm@google.com"
+accountPassword = "354675673"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword, accountCity]);
+
+let accountState;
+
+console.log(accountState);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs , not browser
+
+
+let name = "Ammy"
+let age = 18
+let isLoggedIn = false
+
+console.log(typeof null);
